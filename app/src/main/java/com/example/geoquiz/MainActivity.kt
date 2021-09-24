2f3a61dd6095e9320b83321ae1291035b8cfc9fe
alexyou <s-yualex@bsd405.org>
2021-09-24 10:55:07 -0700

    commit 9/24

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var truebutton: Button
+    private lateinit var falsebutton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        truebutton = findViewById(R.id.true_button)
+        falsebutton = findViewById(R.id.false_button)
+
+        truebutton.setOnClickListener{view: View ->
+            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+
+        }
+        falsebutton.setOnClickListener{view: View ->
+            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        }
     }
 }
