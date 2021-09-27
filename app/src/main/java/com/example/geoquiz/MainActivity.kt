3d0b038bc70b9c57f7f3f3c05764a71339013570
alexyou <s-yualex@bsd405.org>
2021-09-27 10:44:53 -0700

    added next button + question class

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -5,28 +5,41 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     private lateinit var truebutton: Button
     private lateinit var falsebutton: Button
+    private lateinit var nextbutton: Button
+    private lateinit var questionTextView: TextView
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true))
+
+    private var currentIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         truebutton = findViewById(R.id.true_button)
         falsebutton = findViewById(R.id.false_button)
-
+        questionTextView = findViewById(R.id.question_text_view)
         truebutton.setOnClickListener{view: View ->
-            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            var toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
             toast.setGravity(Gravity.TOP, 0, 0)
             toast.show()
 
         }
         falsebutton.setOnClickListener{view: View ->
-            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            var toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
             toast.setGravity(Gravity.TOP, 0, 0)
             toast.show()
         }
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
     }
 }
---
+++ app/src/main/java/com/example/geoquiz/Question.kt
@@ -0,0 +1,3 @@
+package com.example.geoquiz
+
+data class Question (@StringRes val textResId: Int, val answer: Boolean)
